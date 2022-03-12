6a33de3fbdc27b567215834814f2ecae95915e10
Satria Rahmat <sumgmng@gmail.com>
2022-03-12 15:46:04 +0800

    update

--- js/array_divider.js
+++ js/array_divider.js
@@ -17,14 +17,6 @@ function array_divider(count_sublist, data) {
 		result.push(subres);
 	}
 
-	// for x, y in enumerate(list_obj):
-	// 	subres.append(y)
-	// 	if (x+1)%count_subtask == 0:
-	// 		result.append(subres)
-	// 		subres = []
-	// if len(subres) > 0:
-	// 	result.append(subres)
-
 	return result;
 }
 
